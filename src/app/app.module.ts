6100f78a4b1d283e1da063513da30a9dfcfebc65
Rachel Kiarie <rachelnnk94@gmail.com>
2022-07-02 13:38:43 +0300

    Add modal.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { CharitiesComponent } from './charities/charities.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
-    ContactFormComponent
+    ContactFormComponent,
+    CharitiesComponent
   ],
   imports: [
     BrowserModule,
