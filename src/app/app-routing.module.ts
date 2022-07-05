3f2b7f47eaeb8a31ea0d409855d0b5801106ff77
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-07-05 12:54:35 +0300

    ft:created paths for the components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
+import { DonorDashboardComponent } from './pages/donor-dashboard/donor-dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
 import { TeamComponent } from './pages/team/team.component';
+import { TestimonialComponent } from './pages/testimonial/testimonial.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'about',component:AboutComponent},
-  {path:'team',component:TeamComponent}
+  {path:'team',component:TeamComponent},
+  {path:'testimonial',component:TestimonialComponent},
+  {path:'donor-dashboard',component:DonorDashboardComponent}
 ];
 
 @NgModule({
