af03ada17c48835c566736fac1571b50ba612d4d
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-07-05 12:43:02 +0300

    ft:added testimonial & donor dashboard components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { TeamComponent } from './pages/team/team.component';
+import { TestimonialComponent } from './pages/testimonial/testimonial.component';
+import { DonorDashboardComponent } from './pages/donor-dashboard/donor-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    TeamComponent
+    TeamComponent,
+    TestimonialComponent,
+    DonorDashboardComponent
   ],
   imports: [
     BrowserModule,
