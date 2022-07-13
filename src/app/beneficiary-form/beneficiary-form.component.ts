44192c9eb599e937cd770311a007ab12a988df1a
Collins Sirwani <sircollo71@gmail.com>
2022-07-13 22:49:39 +0300

    Fix charities form

--- src/app/beneficiary-form/beneficiary-form.component.ts
+++ src/app/beneficiary-form/beneficiary-form.component.ts
@@ -16,50 +16,52 @@ const httpOptions = {
 })
 export class BeneficiaryFormComponent implements OnInit {
   public countries:any = countries
-  charity:any = this.tokenStorageService.getUser().id;
   currentUser:any
   charities:any
   id:any
   isSuccessful =  false;
   pk:any
+  charity:any
   form: any = {
-    // beneficiary_name: null,
-    // contact: null,
-    // location: null,
-    // country: null,
-    // donation_received: null,
-    // charity:this.charity
+    beneficiary_name: null,
+    contact: null,
+    location: null,
+    country: null,
+    donation_received: null,
+    charity:null,
 
   };
+  
   constructor(private http:HttpClient, private tokenStorageService:TokenStorageService,private router:Router,private charityService:CharityService) { }
 
   ngOnInit(): void {
     this.currentUser = this.tokenStorageService.getUser();
     this.id = this.currentUser.id;
-    console.log(this.id)
     this.getCharity(this.id)
+  
   }
   getCharity(_id:any){
     this.charityService.getCharityDashboardDetails(this.id).subscribe((charities=>{
-      this.charities = charities;
-      console.log(this.charity[0].id)
-      this.id = this.charity[0].id;
-
+      this.charity = charities;
+      // console.log(this.charity[0].id)
+      this.charity = this.charity[0].id;
+      // console.log(this.charity)
     }))
   }
-  postBeneficiary(forms:{beneficiary_name:string,contact:number, location:string,country: string, donation_received:string}) {
-    this.http.post('https://funds-flow.herokuapp.com/charity/'+this.id+'/beneficiaries/', this.form,httpOptions).subscribe((res) => {
+  postBeneficiary(form:{beneficiary_name:string,contact:number, location:string,country: string, donation_received:string,charity:number}) {
+    this.http.post('https://funds-flow.herokuapp.com/charity/1/beneficiaries/', form).subscribe((res) => {
       console.log(res);
       this.isSuccessful = true;
-  })
-    // }, err=>{
-    //   alert(err.error.errorMessage)
-    //   // console.err('error caught in component')
-    // })
-    // this.router.navigate(["/paypal"]).then(() => {
-    //   window.location.reload();
-    // });
-    console.log(forms)
+ 
+    }, err=>{
+      // alert(err.error.errorMessage)
+     
+    })
+    alert('Beneficiary Added Successfully')
+    this.router.navigate(["charity-dashboard"]).then(() => {
+      window.location.reload();
+    });
+    // console.log(this.form.charity[0].id)
   }
 
 }
