99a9020485f3155e92a83d797ada030bf90569c9
Rachel Kiarie <rachelnnk94@gmail.com>
2022-07-13 10:15:10 +0300

    Auto-generated commit [ci skip]

--- src/app/emitters/emitter.ts
+++
@@ -1,6 +0,0 @@
-import { EventEmitter } from '@angular/core';
-
-export class Emiters{
-  static authEmitter= new EventEmitter<boolean>();
-
-}
--- src/app/shared/country.ts
+++
@@ -1,4 +0,0 @@
-export interface Countries {
-  name: string
-
-}
